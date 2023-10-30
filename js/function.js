d304b566e9581feb3a18f541e117798b35eee222
Попова Ксения <ksupop65@mail.ru>
2023-10-30 18:31:22 +0500

    изменения

--- js/function.js
+++ js/function.js
@@ -19,8 +19,8 @@ function checkMeetingTime (start, end, meet, duration) {
   return true;
 }
 
-console.log(checkMeetingTime('08:00', '17:30', '14:00', 90));
-console.log(checkMeetingTime('8:0', '10:0', '8:0', 120));
-console.log(checkMeetingTime('08:00', '14:30', '14:00', 90));
-console.log(checkMeetingTime('14:00', '17:30', '08:0', 90));
-console.log(checkMeetingTime('8:00', '17:30', '08:00', 900));
+checkMeetingTime('08:00', '17:30', '14:00', 90);
+checkMeetingTime('8:0', '10:0', '8:0', 120);
+checkMeetingTime('08:00', '14:30', '14:00', 90);
+checkMeetingTime('14:00', '17:30', '08:0', 90);
+checkMeetingTime('8:00', '17:30', '08:00', 900);
