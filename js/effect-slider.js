5424c79a48e8f206edef3e23d30e88d90bbe9ef3
Попова Ксения <ksupop65@mail.ru>
2023-11-27 00:25:02 +0500

    9.2

---
+++ js/effect-slider.js
@@ -0,0 +1,119 @@
+const slider = document.querySelector('.effect-level__slider');
+const image = document.querySelector('.img-upload__preview img');
+const effectLevel = document.querySelector('.effect-level__value');
+
+noUiSlider.create(slider, {
+  range: {
+    min: 0,
+    max: 1
+  },
+  start: 0,
+  step: 0.1,
+  connect: 'lower',
+});
+
+let effectName = '';
+
+slider.noUiSlider.on('update', () => {
+  const value = slider.noUiSlider.get();
+  effectLevel.value = parseFloat(value);
+  if (effectName === 'none') {
+    image.style.removeProperty('filter');
+  } else {
+    image.style.filter = `${effectName}(${value})`;
+  }
+});
+
+const changeSlider = (evt) => {
+  if (evt.target.closest('input')) {
+    switch (evt.target.value) {
+      case 'chrome':
+      case 'sepia':
+        effectName = evt.target.value === 'chrome' ? 'grayscale' : 'sepia';
+        slider.noUiSlider.updateOptions ({
+          range: {
+            min: 0,
+            max: 1
+          },
+          start: 1,
+          step: 0.1,
+          format: {
+            to: function (value) {
+              return value;
+            },
+            from: function (value) {
+              return value;
+            },
+          }
+        });
+        slider.parentNode.classList.remove('hidden');
+        break;
+      case 'marvin':
+        effectName = 'invert';
+        slider.noUiSlider.updateOptions ({
+          range: {
+            min: 0,
+            max: 100
+          },
+          start: 100,
+          step: 1,
+          format: {
+            to: function (value) {
+              return `${value}%`;
+            },
+            from: function (value) {
+              return value;
+            },
+          }
+        });
+        slider.parentNode.classList.remove('hidden');
+        break;
+      case 'phobos':
+        effectName = 'blur';
+        slider.noUiSlider.updateOptions ({
+          range: {
+            min: 0,
+            max: 3
+          },
+          start: 3,
+          step: 0.1,
+          format: {
+            to: function (value) {
+              return `${value}px`;
+            },
+            from: function (value) {
+              return value;
+            },
+          }
+        });
+        slider.parentNode.classList.remove('hidden');
+        break;
+      case 'heat':
+        effectName = 'brightness';
+        slider.noUiSlider.updateOptions ({
+          range: {
+            min: 1,
+            max: 3
+          },
+          start: 3,
+          step: 0.1,
+          format: {
+            to: function (value) {
+              return value;
+            },
+            from: function (value) {
+              return value;
+            },
+          }
+        });
+        slider.parentNode.classList.remove('hidden');
+        break;
+      case 'none':
+        effectName = 'none';
+        slider.noUiSlider.set(0);
+        slider.parentNode.classList.add('hidden');
+    }
+  }
+};
+
+export {changeSlider};
--- js/photo-upload-form.js
+++ js/photo-upload-form.js
@@ -1,4 +1,5 @@
 import {isEscKey} from './util.js';
+import {changeSlider as effectSlider} from './effect-slider.js';
 
 const uploadForm = document.querySelector('.img-upload__form');
 const uploadInput = uploadForm.querySelector('.img-upload__input');
@@ -6,22 +7,45 @@ const formToEditPhoto = uploadForm.querySelector('.img-upload__overlay');
 const closeFormButton = uploadForm.querySelector('.img-upload__cancel');
 const hashtagInput = uploadForm.querySelector('.text__hashtags');
 const commentInput = uploadForm.querySelector('.text__description');
+const listOfEffects = uploadForm.querySelector('.effects__list');
+const sizeInput = uploadForm.querySelector('.scale__control--value');
+const image = uploadForm.querySelector('.img-upload__preview img');
 
 const MAX_COMMENT_LENGTH = 140;
 const MAX_COUNT_HASHTAGS = 5;
 const hashtagFormat = /^#[a-zа-яё0-9]{1,19}$/i;
+const ZOOM_STEP = 25;
 
 const closeByEscape = (evt) => evt.stopPropagation();
 hashtagInput.addEventListener('keydown', closeByEscape);
 commentInput.addEventListener('keydown', closeByEscape);
 
+const changeSize = (evt) => {
+  let newValue = parseInt(sizeInput.value, 10);
+  if(evt.target.classList.contains('scale__control--bigger')) {
+    if (newValue + ZOOM_STEP > 100) { newValue = 100; }
+    else {newValue += ZOOM_STEP; }
+  }
+  if(evt.target.classList.contains('scale__control--smaller')) {
+    if (newValue - ZOOM_STEP < ZOOM_STEP) { newValue = ZOOM_STEP; }
+    else { newValue -= ZOOM_STEP; }
+  }
+  sizeInput.value = `${newValue}%`;
+  image.style.transform = `scale(${newValue === 100 ? '1' : `0.${newValue}`})`;
+};
+
+
 const closeForm = () => {
   formToEditPhoto.classList.add('hidden');
   document.body.classList.remove('modal-open');
-
   closeFormButton.removeEventListener('click', closeForm);
   document.removeEventListener('keydown', closeFormByEsc);
-
+  hashtagInput.removeEventListener('keydown', closeByEscape);
+  commentInput.removeEventListener('keydown', closeByEscape);
+  listOfEffects.removeEventListener('click', effectSlider);
+  uploadForm.querySelector('.img-upload__scale').removeEventListener('click', changeSize);
+  image.style.removeProperty('transform');
+  image.style.removeProperty('filter');
   uploadForm.reset();
 };
 
@@ -34,9 +58,13 @@ function closeFormByEsc (evt) {
 const openForm = () => {
   formToEditPhoto.classList.remove('hidden');
   document.body.classList.add('modal-open');
-
+  document.querySelector('.img-upload__effect-level').classList.add('hidden');
   closeFormButton.addEventListener('click', closeForm);
   document.addEventListener('keydown', closeFormByEsc);
+  hashtagInput.addEventListener('keydown', closeByEscape);
+  commentInput.addEventListener('keydown', closeByEscape);
+  listOfEffects.addEventListener('click', effectSlider);
+  uploadForm.querySelector('.img-upload__scale').addEventListener('click', changeSize);
 };
 
 uploadInput.addEventListener('change', openForm);
