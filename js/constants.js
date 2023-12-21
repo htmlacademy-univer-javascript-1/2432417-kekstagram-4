b2d92bcbae657776fcc2292a4cdff84e0271db13
Попова Ксения <ksupop65@mail.ru>
2023-12-21 19:03:51 +0500

    исправления

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,7 @@
 const COMMENTS_COUNT = 5;
 const MAX_COMMENT_LENGTH = 140;
 const MAX_COUNT_HASHTAGS = 5;
-const HASHTAGFORMAT = /^#[a-zа-яё0-9]{1,19}$/i;
+const HASHTAG_FORMAT = /^#[a-zа-яё0-9]{1,19}$/i;
 const TIME_TO_DELETE_MESSAGE = 5000;
 const RANDOM_PHOTOS_LENGTH = 10;
 const DELAY = 500;
@@ -15,7 +15,7 @@ const ZOOM = {
 
 const BASE_URLS = {
   GET: 'https://29.javascript.pages.academy/kekstagram/data',
-  POST: 'https://29.javascript.pages.academy/kekstagram/',
+  POST: 'https://29.javascript.pages.academy/kekstagram',
 };
 
-export { COMMENTS_COUNT, MAX_COMMENT_LENGTH, MAX_COUNT_HASHTAGS, HASHTAGFORMAT, ZOOM, TIME_TO_DELETE_MESSAGE, RANDOM_PHOTOS_LENGTH, DELAY, TYPES_OF_FILE, BASE_URLS };
+export { COMMENTS_COUNT, MAX_COMMENT_LENGTH, MAX_COUNT_HASHTAGS, HASHTAG_FORMAT, ZOOM, TIME_TO_DELETE_MESSAGE, RANDOM_PHOTOS_LENGTH, DELAY, TYPES_OF_FILE, BASE_URLS };
